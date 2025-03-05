abf38d15ccfb35f0776be1be1aa3fce5c9160c82
NguyenVanTu2k2 <vantu29032002@gmail.com>
2025-03-05 21:57:54 +0700

    gitlab->github

---
+++ src/main/java/selling/electronic_devices/ElectronicDevicesApplication.java
@@ -0,0 +1,13 @@
+package selling.electronic_devices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicDevicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicDevicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/selling/electronic_devices/ElectronicDevicesApplicationTests.java
@@ -0,0 +1,13 @@
+package selling.electronic_devices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicDevicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
